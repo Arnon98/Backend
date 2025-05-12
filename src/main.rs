8a108d4c8ce4c62d55e35857d210d361b6507106
M.Arnon <arnon.miang@gmail.com>
2025-05-12 21:57:50 +0700

    V0.0.0.3 set http

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use Backend::config::config_loader;
+use std::sync::Arc;
+
+use Backend::{config::config_loader, infrastructure::{axum_http::http_serve::start, postgres::postgres_connection}};
 use tracing::{error, info};
 #[tokio::main]
 async fn main(){
@@ -16,4 +18,18 @@ async fn main(){
 
     //info!("ENV has been loaded: {:?}", dotenvy_env); | debug
     info!("ENV has been loaded:");
+
+    let postgres_pool = match postgres_connection::establish_connect(&dotenvy_env.database.url){
+        Ok(pool) => pool,
+        Err(e)=>{
+            error!("Failed to establish connect to postgres: {}",e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("Postgres connection has been established");
+
+    start(Arc::new(dotenvy_env), Arc::new(postgres_pool))
+    .await
+    .expect("Failed to start server");
 }
